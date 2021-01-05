f8aed8f7987a680ceb01b761e87cde5e9fec098a
AhmedAbozaid9 <52420168+AhmedAbozaid9@users.noreply.github.com>
2021-01-06 00:02:51 +0200

    Update script.js
    add all the js

--- Progress steps/script.js
+++ Progress steps/script.js
@@ -1,8 +1,48 @@
 let prev = document.querySelector('.prev');
+let next = document.querySelector('.next');
 let progress = document.querySelector('.progressLine');
-let width = window.getComputedStyle(progress).width;
-width = '100px'
-console.log(width);
-// prev.addEventListener('click',function(){
 
-// })
+let activeCircles = 1;
+//activate and deactivate the buttons
+
+function deactivate(){
+    if(activeCircles === 1){
+        prev.disabled = true;
+        next.disabled = false;
+    } else if(activeCircles === 4){
+        prev.disabled = false;
+        next.disabled = true;
+    }else{
+        prev.disabled = false;
+        next.disabled = false;
+    }
+}
+//update the website
+function update(){
+    let circles = document.querySelectorAll('.circle');
+    for(let i = 0; i < circles.length;i++){
+        if(i<activeCircles){
+            circles[i].classList.add('active');
+        }else if(i>=activeCircles){
+            circles[i].classList.remove('active');
+        }
+    }
+    progress.style.width = ((activeCircles - 1) / 3) * 100 + "%";
+}
+//add event listeners
+next.addEventListener('click',function(){
+    activeCircles++;
+    if(activeCircles > 4){
+        activeCircles = 4;
+    }
+    deactivate();
+    update()
+})
+prev.addEventListener('click',function(){
+    activeCircles--;
+    if(activeCircles < 1){
+        activeCircles = 1;
+    }
+    deactivate();
+    update()
+})
