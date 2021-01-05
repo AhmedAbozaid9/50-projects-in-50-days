13b6b474f2303ddf59fa0a8bf38c13f5927fe293
AhmedAbozaid9 <52420168+AhmedAbozaid9@users.noreply.github.com>
2021-01-05 15:50:55 +0200

    Update script.js
    i made the cards expand automatically

--- Expanding cards/script.js
+++ Expanding cards/script.js
@@ -5,5 +5,46 @@ for(let i = 0;i < slides.length;i++){
            slides[j].classList.remove('active');
        }
        slides[i].classList.add('active');
-    })
-}    
+        counter = parseInt(this.classList[0].substring(5,));
+       if(counter === 5){
+        setTimeout(function(){
+            counter = 4;
+            check = false;
+        })
+    }
+    },1000)
+}  
+//auto transaction
+let counter = 1;
+let check = true;
+setInterval(function(){
+    if(counter < 5 && check){
+        slides[counter - 1].classList.remove('active');
+        slides[counter].classList.add('active');
+        counter++;
+        if(counter === 5){
+            setTimeout(function(){
+                check = false;
+            slides[3].classList.remove('active');
+            slides[4].classList.add('active');
+            counter = 4;
+            },1000)
+        }
+    }
+   
+
+    
+    if(!check){
+        
+        slides[counter].classList.remove('active');
+        slides[counter -1].classList.add('active');
+        counter--;
+        
+        if(counter === 0){
+            check = true;
+            counter = 1;
+        }
+        
+    }
+    
+},3000)
