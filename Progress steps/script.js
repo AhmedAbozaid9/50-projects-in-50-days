fd2f92b5185afbf4284a4480999e07d79a7e0784
AhmedAbozaid9 <52420168+AhmedAbozaid9@users.noreply.github.com>
2021-01-05 23:01:08 +0200

    Add files via upload
    added the HTML and CSS for the progress steps

---
+++ Progress steps/script.js
@@ -0,0 +1,8 @@
+let prev = document.querySelector('.prev');
+let progress = document.querySelector('.progressLine');
+let width = window.getComputedStyle(progress).width;
+width = '100px'
+console.log(width);
+// prev.addEventListener('click',function(){
+
+// })
